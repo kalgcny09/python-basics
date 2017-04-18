14318a4e01c81689fbaa44e56ba84d11954e3231
kalgcny09 <kalgcny@gmail.com>
2017-04-18 15:12:29 -0400

    Next attempt

--- python101.py
+++ python101.py
@@ -1,6 +1,10 @@
-print "Hello World" 
-print ("Hello World", "Again")
-print """This
-will ignore the new lines
-until it sees another three double quotes"""
+first_name = raw_input("First Name: ")
+last_name = raw_input("Last Name: ")
+age = raw_input("How old are you?")
+age_as_int = int(age)
 
+if (first_name== last_name):
+	print "Your first name is the same as your last name?"
+
+if (age >= 21):
+	print "you can buy beer"
